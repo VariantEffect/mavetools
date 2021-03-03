92e8e6de4c5bc667439a926f6bb8de0613c5d63a
Joseph Min <josephmin@gmail.com>
2021-03-03 11:32:14 -0500

    for travis

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     install_requires=requirements,
     test_suite="tests",
 )
