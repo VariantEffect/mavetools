cd9323fa42050a2c1205820634d1c2ab8e7eb51e
harmatt <79935163+harmatt@users.noreply.github.com>
2021-03-30 15:53:41 -0700

    merge functions and reformat

--- mavetools/mavedf/df_to_pandas.py
+++ mavetools/mavedf/df_to_pandas.py
@@ -1,8 +1,10 @@
+import _io
+
 import pandas as pd
 from csv import reader
 
 
-def df_to_pandas(df):
+def df_to_pandas(df, ret_meta=False):
     """
     This function converts a dataframe downloaded from MaveDB into a pandas dataframe and returns
     the new data frame. The user has the option to drop the accession numbers.
@@ -10,10 +12,13 @@ def df_to_pandas(df):
     Parameters
     ----------
     df (string): the filename.csv to be converted to pandas df
+    ret_meta (bool): True if user wants metadata
+        default value = False
 
     Returns
     -------
     df_pandas (pandas.df): the converted df
+    meta_dict (dictionary): dictionary containing formatted metadata (comments) at the top of df
 
     Raises
     ______
@@ -25,61 +30,44 @@ def df_to_pandas(df):
     """
     # check for TypeError
     # if first argument is not string
-    #if not isinstance(df, str):
-    #    raise TypeError("df must be string")
-    # if no arguments are passed
-    #if df is None:  #************** remove this
-    #    raise TypeError("no arguments passed")
+    if not isinstance(df, str):
+        if not isinstance(df, _io.StringIO):  # account for StringIO object in test file
+            raise TypeError("df must be string in filename.csv format")
 
     # check for ValueError
     # if df is not in filename.csv format
-    #if not df.endswith(".csv"):
-    #    raise ValueError("df must be csv file")
-
-    # convert df to pandas df
-    df_pandas = pd.read_csv(df, skiprows=4)
-
-    # return df_pandas and meta_dict
-    return df_pandas
-
-
-def get_meta_data(df):
-    """
-    This function takes in a df and returns a dictionary with the formatted metadata.
-
-    Parameters
-    ----------
-    df (string): the filename.csv to be converted to pandas df
-
-    Returns
-    -------
-    meta_dict (dictionary): dictionary containing formatted metadata (comments) at the top of df
-
-    Raises
-    ______
-
-    """
+    if not isinstance(df, _io.StringIO):  # check first that it is not StringIO object for testing
+        if not df.endswith(".csv"):
+            raise ValueError("df must be csv file")
 
     # store metadata in dictionary
     # declare dictionary
     meta_dict = {}
 
-    # open csv file in read mode
-    with open("urn mavedb 00000001-a-1_scores.csv", "r") as mave_data:  # *********** dNOT hardcode this
-        csv_reader = reader(mave_data)
-        # read line by line and check for metadata
-        for row in csv_reader:
-            if not row[0].startswith("#"):  # we have found all the metadata
-                break
-            else:
-                # get the metadata at that index
-                get_meta = row[0]
-                # remove '#' symbol
-                get_meta = get_meta[2:]
-                # break string into key/value pairs
-                split_meta = get_meta.split(":", 1)
-                # add to meta_dict
-                meta_dict.update({split_meta[0]: split_meta[1][1:]})
+    # open csv file in read mode, if it's a csv file
+    if isinstance(df, str):  # df is a filename.csv file
+        mave_data = open(df, "r")
+    elif isinstance(df, _io.StringIO):  # df is a StringIO object for testing
+        mave_data = df
+
+    # get metadata
+    for row in range(4):
+        get_meta = mave_data.readline()
+        # remove '#' symbol
+        get_meta = get_meta[2:]
+        # break string into key/value pairs
+        split_meta = get_meta.split(":", 1)
+        # add to meta_dict
+        meta_dict.update({split_meta[0]: split_meta[1][1:]})
+
+    # convert df to pandas df
+    df_pandas = pd.read_csv(df)
+
+    # close file
+    mave_data.close()
 
     # return df_pandas and meta_dict
-    return meta_dict
+    if not ret_meta:
+        return df_pandas
+    else:
+        return df_pandas, meta_dict
