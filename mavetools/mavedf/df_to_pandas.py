20d1e37b50397514cf5c29c91f1a9f87be48a23c
harmatt <79935163+harmatt@users.noreply.github.com>
2021-03-30 13:03:36 -0700

    refactor

--- mavetools/mavedf/df_to_pandas.py
+++ mavetools/mavedf/df_to_pandas.py
@@ -2,67 +2,74 @@ import pandas as pd
 from csv import reader
 
 
-def df_to_pandas(df, drop_accession=False):
+def df_to_pandas(df):
     """
     This function converts a dataframe downloaded from MaveDB into a pandas dataframe and returns
-    the new data frame and a dictionary with the formatted metadata. The user has the option to
-    drop the accession numbers.
+    the new data frame. The user has the option to drop the accession numbers.
 
     Parameters
     ----------
     df (string): the filename.csv to be converted to pandas df
-    drop_accession (bool): True if user wants to drop the accession numbers
-        default value = False
 
     Returns
     -------
     df_pandas (pandas.df): the converted df
-    meta_dict (dictionary): dictionary containing formatted metadata (comments) at the top of df
 
     Raises
     ______
     TypeError:
-        if first argument is not string
-        if second argument is not bool
+        if argument is not string
         if no arguments are passed
     ValueError:
         if first argument is not in filename.csv format
     """
     # check for TypeError
     # if first argument is not string
-    if not isinstance(df, str):
-        raise TypeError("df must be string")
-    # if drop_accession is not bool
-    if not isinstance(drop_accession, bool):
-        raise TypeError("drop_accession must be boolean value")
+    #if not isinstance(df, str):
+    #    raise TypeError("df must be string")
     # if no arguments are passed
-    if df is None:
-        raise TypeError("no arguments passed")
+    #if df is None:  #************** remove this
+    #    raise TypeError("no arguments passed")
 
     # check for ValueError
     # if df is not in filename.csv format
-    if not df.endswith(".csv"):
-        raise ValueError("df must be csv file")
+    #if not df.endswith(".csv"):
+    #    raise ValueError("df must be csv file")
 
     # convert df to pandas df
     df_pandas = pd.read_csv(df, skiprows=4)
 
-    # remove accession numbers if drop_accession = True
-    if drop_accession:
-        # make sure accession numbers exist
-        if "accession" in df_pandas:
-            del df_pandas["accession"]
+    # return df_pandas and meta_dict
+    return df_pandas
+
+
+def get_meta_data(df):
+    """
+    This function takes in a df and returns a dictionary with the formatted metadata.
+
+    Parameters
+    ----------
+    df (string): the filename.csv to be converted to pandas df
+
+    Returns
+    -------
+    meta_dict (dictionary): dictionary containing formatted metadata (comments) at the top of df
+
+    Raises
+    ______
+
+    """
 
     # store metadata in dictionary
     # declare dictionary
     meta_dict = {}
 
     # open csv file in read mode
-    with open("urn mavedb 00000001-a-1_scores.csv", "r") as mave_data:
+    with open("urn mavedb 00000001-a-1_scores.csv", "r") as mave_data:  # *********** dNOT hardcode this
         csv_reader = reader(mave_data)
         # read line by line and check for metadata
         for row in csv_reader:
-            if row[0].startswith("#") == False: # we have found all the metadata
+            if not row[0].startswith("#"):  # we have found all the metadata
                 break
             else:
                 # get the metadata at that index
@@ -75,18 +82,4 @@ def df_to_pandas(df, drop_accession=False):
                 meta_dict.update({split_meta[0]: split_meta[1][1:]})
 
     # return df_pandas and meta_dict
-    return df_pandas, meta_dict
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return meta_dict
