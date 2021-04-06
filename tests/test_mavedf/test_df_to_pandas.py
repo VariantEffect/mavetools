9533f196b3eeef31800e0687734d2f49321c62c1
harmatt <79935163+harmatt@users.noreply.github.com>
2021-04-05 17:29:59 -0700

    add test cases

--- tests/test_mavedf/test_df_to_pandas.py
+++ tests/test_mavedf/test_df_to_pandas.py
@@ -6,10 +6,9 @@ from mavetools.mavedf.df_to_pandas import df_to_pandas
 
 class Test(TestCase):
 
-    # scenario 1 - call function on urn mavedb 00000001-a-1_scores
-
-    # typical case - first argument is valid
+    # scenario 1 - valid input file
     def test_df_to_pandas(self):
+        # typical case - return metadata
         data = StringIO("# Accession:\n"
                         "# Downloaded (UTC):\n"
                         "# Licence:\n"
@@ -19,16 +18,34 @@ class Test(TestCase):
         results = df_to_pandas(data, True)
         self.assertTrue(results[0].iat[0, 2] == "p.Phe2=")
 
-    # scenario 2 - call function on invalid filename
+        # typical case - do not return metadata
+        data = StringIO("# Accession:\n"
+                        "# Downloaded (UTC):\n"
+                        "# Licence:\n"
+                        "# Licence URL:\n"
+                        "accession,hgvs_nt,hgvs_pro,score\n"
+                        "urn:mavedb:00000011-a-1#27,c.[4=;5=;6=],p.Phe2=,0.0")
+        results = df_to_pandas(data, False)
+        self.assertTrue(results.iat[0, 2] == "p.Phe2=")
 
-    # edge case - invalid first argument
+        # edge case - ret_meta is incorrect type
+        data = StringIO("# Accession:\n"
+                        "# Downloaded (UTC):\n"
+                        "# Licence:\n"
+                        "# Licence URL:\n"
+                        "accession,hgvs_nt,hgvs_pro,score\n"
+                        "urn:mavedb:00000011-a-1#27,c.[4=;5=;6=],p.Phe2=,0.0")
+        with self.assertRaises(TypeError):
+            df_to_pandas(data, "notabool")
+
+    # scenario 2 - invalid filename
     def test_df_to_pandas_invalid_first_arg(self):
+        # edge case - invalid filename
         with self.assertRaises(ValueError):
             df_to_pandas("notarealfile")
 
-    # scenario 3 - call function without arguments
-
-    # edge case - missing first argument
+    # scenario 3 - missing arguments
     def test_df_to_pandas_missing_arguments(self):
+        # edge case - missing first argument
         with self.assertRaises(TypeError):
             df_to_pandas()
