6e88fa6e0c3b57ceb91d6c78c58565ebdc5f5466
harmatt <79935163+harmatt@users.noreply.github.com>
2021-09-15 18:49:25 -0700

    licence comments

--- mavetools/models/licence.py
+++ mavetools/models/licence.py
@@ -2,10 +2,16 @@ import attr
 
 @attr.s
 class Licence():
+    """
+    Class contains attributes of Licence
+    """
     long_name: str = attr.ib(kw_only=True, default=None)
     short_name: str = attr.ib(kw_only=True)
     link: str = attr.ib(kw_only=True, default=None)
     version: str = attr.ib(kw_only=True, default=None)
 
     def valid_short_names():
+        """
+        Returns list of valid Licence short names
+        """
         return ['CC0', 'CC BY-NC-SA 4.0', 'CC BY 4.0']
