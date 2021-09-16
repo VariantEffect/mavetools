bddbb68100d4b557e6b6d2a56bc7e963db38cb0c
harmatt <79935163+harmatt@users.noreply.github.com>
2021-09-15 18:54:59 -0700

    target comments

--- mavetools/models/target.py
+++ mavetools/models/target.py
@@ -7,16 +7,25 @@ from .external_identifier import ExternalIdentifier
 
 @attr.s
 class ReferenceSequence():
+    """
+    Instantiates ReferenceSequence object and declares attributes
+    """
     sequence: str = attr.ib(kw_only=True)
 
 
 @attr.s
 class SequenceOffset(ExternalIdentifier):
+    """
+    Instantiates SequenceOffset and declares attributes
+    """
     offset: int = attr.ib(kw_only=True)
 
 
 @attr.s
 class ReferenceGenome():
+    """
+    Instantiates SequenceOffset and declares attributes
+    """
     short_name: str = attr.ib(kw_only=True)
     organism_name: str = attr.ib(kw_only=True, default=None)
     assembly_identifier: ExternalIdentifier = attr.ib(kw_only=True, default=None)
@@ -24,11 +33,17 @@ class ReferenceGenome():
 
 @attr.s
 class ReferenceMap():
+    """
+    Instantiates ReferenceMap object and declares genome attribute
+    """
     genome: ReferenceGenome = attr.ib(kw_only=True)
 
 
 @attr.s
 class Target():
+    """
+    Instantiates target object and declares attributes
+    """
     name: str = attr.ib(kw_only=True)
     reference_sequence: ReferenceSequence = attr.ib(kw_only=True)
     uniprot: SequenceOffset = attr.ib(kw_only=True)
@@ -42,6 +57,9 @@ class Target():
 
 @attr.s
 class NewTarget():
+    """
+    Instantiates NewTarget object and declares attributes and functions
+    """
     # existing_target: str = attr.ib(kw_only=True, default=None)
     name: str = attr.ib(kw_only=True)
     reference_sequence: Optional[ReferenceSequence] = attr.ib(kw_only=True, default=None)
@@ -53,7 +71,13 @@ class NewTarget():
     fasta_file: str = attr.ib(kw_only=True, default=None)
 
     def valid_types():
+        """
+        Returns list of valid target types
+        """
         return ['Protein coding', 'Regulatory', 'Other noncoding']
 
     def valid_sequence_types():
+        """
+        Returns list of valid sequence types
+        """
         return ['Infer', 'DNA', 'Protein']
