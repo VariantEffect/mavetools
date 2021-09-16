7365957dbda99cea8d97ea28974f1a137514edef
harmatt <79935163+harmatt@users.noreply.github.com>
2021-09-15 18:48:08 -0700

    dataset comments

--- mavetools/models/dataset.py
+++ mavetools/models/dataset.py
@@ -6,29 +6,42 @@ from .external_identifier import ExternalIdentifier
 
 @attr.s
 class Urn():
+    """
+    Instantiates the Urn object and declares urn attribute
+    """
     urn: str = attr.ib(kw_only=True)
 
 
 @attr.s
 class Keyword():
+    """
+    Instantiates the Keyword object and declares text attribute
+    """
     text: str = attr.ib(kw_only=True)
 
 
 @attr.s
 class TimeStamped():
+    """
+    Instantiates TimeStamped object and declares attributes
+    """
     creation_date: str = attr.ib(kw_only=True)
     modification_date: str = attr.ib(kw_only=True)
 
 
 @attr.s
 class Dataset(TimeStamped, Urn):
+    """
+    Instantiates Dataset object and declares attributes
+    """
     publish_date: str = attr.ib(kw_only=True)
     created_by: str = attr.ib(kw_only=True)
     modified_by: str = attr.ib(kw_only=True)
+    # optional attributes
     approved: Optional[str] = attr.ib(kw_only=True, default=None)
     private: Optional[bool] = attr.ib(kw_only=True, default=None)
     last_child_value: Optional[Any] = attr.ib(kw_only=True, default=None)
-
+    # optional attribute
     extra_metadata: Optional[Dict[str, str]] = attr.ib(kw_only=True, default=None)
     abstract_text: str = attr.ib(kw_only=True)
     method_text: str = attr.ib(kw_only=True)
@@ -41,13 +54,18 @@ class Dataset(TimeStamped, Urn):
     contributors: List[str] = attr.ib(kw_only=True)
 
     def deserialize():
+        # why pass?
         pass
 
 
 @attr.s
 class NewDataset():
+    """
+    Instantiates NewDataset object and declares object attributes
+    """
     title: str = attr.ib(kw_only=True)
     short_description: str = attr.ib(kw_only=True)
+    # optional attributes
     abstract_text: Optional[str] = attr.ib(kw_only=True, default=None)
     method_text: Optional[str] = attr.ib(kw_only=True, default=None)
     keywords: Optional[List[Keyword]] = attr.ib(kw_only=True, default=None)
