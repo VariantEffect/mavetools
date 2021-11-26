37f3ba29b90292d7bf531973aed39af365afa3d8
Alan Rubin <alan.rubin@wehi.edu.au>
2021-11-26 22:42:24 +1100

    move requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,15 @@ import sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt") as f:
-    requirements = f.readlines()
+requirements = [
+    "attrs",
+    "requests",
+    "fqfa>=1.2.1",
+    "mavehgvs",
+    "numpy",
+    "pandas",
+    "idutils",
+]
 
 # fqfa requires backported dataclasses in Python 3.6
 if sys.version_info.major == 3 and sys.version_info.minor == 6:
