1621814390ddb9801ea01d3dc6e0d5cc17441b09
Alan Rubin <alan.rubin@wehi.edu.au>
2021-11-30 15:01:23 +1100

    increment version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if sys.version_info.major == 3 and sys.version_info.minor == 6:
 
 setup(
     name="mavetools",
-    version="0.1.0",
+    version="0.1.1",
     author="Alan F Rubin",
     author_email="alan.rubin@wehi.edu.au",
     description=(
