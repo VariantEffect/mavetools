0c13b64c6be432925ac4dcb72a1ce829512e192a
harmatt <79935163+harmatt@users.noreply.github.com>
2021-12-21 10:51:26 -0800

    doc string

--- mavetools/misc_tools/delins_to_subs.py
+++ mavetools/misc_tools/delins_to_subs.py
@@ -1,3 +1,28 @@
 
 def delins_to_subs(target, delins, offset=0):
+    """
+    The code that converts codon changes to mavehgvs variants prefers to define single events,
+    e.g. a deletion-insertion of two bases rather than two substitutions. Many users may prefer
+    to look at these data as multiple substitutions instead.
+
+    This function takes delins mavehgvs variants that have matching deletion and insertion length
+    and outputs the corresponding multi-variant.
+
+    For many variants this will require the user to provide the appropriate target sequence.
+    To improve usability, the user is able to provide a longer target sequence and an offset.
+
+    The function should throw an informative error if the delins is not matched or if the target
+    sequence doesn't match (e.g. outside of length bounds, or would result in a target-identical
+    change that suggests the wrong target sequence was provided).
+
+    Parameters
+    ----------
+    target
+    delins
+    offset
+
+    Returns
+    -------
+
+    """
     return None
