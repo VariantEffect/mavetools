3f313eee097ed7922c7cbfde4d2eb09b0a84b51a
harmatt <79935163+harmatt@users.noreply.github.com>
2021-12-21 12:04:32 -0800

    add error catching

--- mavetools/misc_tools/delins_to_subs.py
+++ mavetools/misc_tools/delins_to_subs.py
@@ -11,10 +11,6 @@ def delins_to_subs(target, delins, offset=0):
     For many variants this will require the user to provide the appropriate target sequence.
     To improve usability, the user is able to provide a longer target sequence and an offset.
 
-    The function should throw an informative error if the delins is not matched or if the target
-    sequence doesn't match (e.g. outside of length bounds, or would result in a target-identical
-    change that suggests the wrong target sequence was provided).
-
     Parameters
     ----------
     target : str
@@ -22,19 +18,21 @@ def delins_to_subs(target, delins, offset=0):
     delins : str
         A mavehgvs representation of a single deletion-insertion event.
     offset : int
-        The offset of index into target sequence. E.g., you have variants from MaveDB that are part of an
+        The offset of index into target sequence (e.g., you have variants from MaveDB that are part of an
         internal domain of a gene. MaveDB's base 1 is actually base 121. In this case, the user can pass
-        as target the full-length FASTA and as offset 120.
+        as target the full-length FASTA and as offset 120).
 
     Returns
     -------
-    str
-        the corresponding multi-variant based on the delins single event
+    multi_variant : str
+        The corresponding multi-variant based on the delins single event
 
     Raises
     ______
-
-
+    ValueError
+        If the target sequence is identical to the variant sequence.
+    ValueError
+        If the variant sequence is outside the length of bounds of the target sequence.
     """
     # save first part of delins string and append start of return string
     multi_variant = delins[0:2] + "["
@@ -44,11 +42,19 @@ def delins_to_subs(target, delins, offset=0):
     start_delins = int(''.join(i for i in start_delins if i.isdigit()))
     end_delins = int(''.join(i for i in end_delins if i.isdigit()))
 
+    # throw error if the end of variant is outside the bounds of the target
+    if end_delins + offset > len(target):
+        raise ValueError("variant outside of length of bounds of target")
+
     # save bases in delins
     bases_delins = ''.join(i for i in delins if i.isupper())
 
     # with range, get appropriate subsequence from target
     bases_target = target[start_delins-1+offset:end_delins+offset]
+
+    # throw error if bases_delins and bases_target are identical
+    if bases_delins == bases_target:
+        raise ValueError("variant sequence matches target sequence")
 
     # compare subsequence in target with bases in delins
     for i in range(len(bases_target)):
