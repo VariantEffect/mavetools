ba5e6e65dfa9485d11ccf9a69519c9e07710232e
harmatt <79935163+harmatt@users.noreply.github.com>
2021-12-22 13:00:54 -0800

    add variant list

--- tests/test_misc_tools/test_infer_target_seq.py
+++ tests/test_misc_tools/test_infer_target_seq.py
@@ -1,9 +1,16 @@
 from unittest import TestCase
 from mavetools.misc_tools import infer_target_seq
+from mavehgvs.variant import Variant
 
 
 class Test(TestCase):
     def test_target_seq_inference(self):
         target_seq = "CAATTTGGTTGGTCTGCTAATATGGAA"
-        variant_list = []
+        variant_list = [Variant("1C>A"), Variant("2A>C"), Variant("3A>C"), Variant("4T>A"), Variant("5T>A"),
+                        Variant("6T>A"), Variant("7G>A"), Variant("8G>A"), Variant("9T>A"), Variant("10T>A"),
+                        Variant("11G>A"), Variant("12G>A"), Variant("13T>A"), Variant("14C>A"), Variant("15T>A"),
+                        Variant("16G>A"), Variant("17C>A"), Variant("18T>A"), Variant("19A>C"), Variant("20A>C"),
+                        Variant("21T>A"), Variant("22A>C"), Variant("23T>A"), Variant("24G>A"), Variant("25G>A"),
+                        Variant("26A>C"), Variant("27A>C")]
+
         self.assertEqual(infer_target_seq.infer_target_seq(variant_list), target_seq)
