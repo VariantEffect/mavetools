cc0a27571b996e7605f2b8c073689e54ce9c4c35
harmatt <79935163+harmatt@users.noreply.github.com>
2021-12-22 14:35:18 -0800

    add test cases

--- tests/test_misc_tools/test_infer_target_seq.py
+++ tests/test_misc_tools/test_infer_target_seq.py
@@ -4,10 +4,45 @@ from mavehgvs.variant import Variant
 
 
 class Test(TestCase):
-    def test_target_seq_inference(self):
+    def test_all_single_sub(self):
         target_seq = "CAATTTGGTTGGTCTGCTAATATGGAA"
         variant_list = [Variant("n.1C>A"), Variant("n.2A>C"), Variant("n.3A>C"), Variant("n.4T>A"),
                         Variant("n.5T>A"), Variant("n.6T>A"), Variant("n.7G>A"), Variant("n.8G>A"),
+                        Variant("n.9T>A"), Variant("n.10T>A"), Variant("n.11G>A"), Variant("n.12G>A"),
+                        Variant("n.13T>A"), Variant("n.14C>A"), Variant("n.15T>A"), Variant("n.16G>A"),
+                        Variant("n.17C>A"), Variant("n.18T>A"), Variant("n.19A>C"), Variant("n.20A>C"),
+                        Variant("n.21T>A"), Variant("n.22A>C"), Variant("n.23T>A"), Variant("n.24G>A"),
+                        Variant("n.25G>A"), Variant("n.26A>C"), Variant("n.27A>C")]
+
+        self.assertEqual(infer_target_seq.infer_target_seq(variant_list), target_seq)
+
+    def test_multi_sub(self):
+        target_seq = "CAATTTGGTTGGTCTGCTAATATGGAA"
+        variant_list = [Variant("n.[1C>A,3A>C]"), Variant("n.2A>C"), Variant("n.4T>A"),
+                        Variant("n.5T>A"), Variant("n.6T>A"), Variant("n.7G>A"), Variant("n.8G>A"),
+                        Variant("n.9T>A"), Variant("n.10T>A"), Variant("n.11G>A"), Variant("n.12G>A"),
+                        Variant("n.13T>A"), Variant("n.14C>A"), Variant("n.15T>A"), Variant("n.16G>A"),
+                        Variant("n.17C>A"), Variant("n.18T>A"), Variant("n.19A>C"), Variant("n.20A>C"),
+                        Variant("n.21T>A"), Variant("n.22A>C"), Variant("n.23T>A"), Variant("n.24G>A"),
+                        Variant("n.25G>A"), Variant("n.26A>C"), Variant("n.27A>C")]
+
+        self.assertEqual(infer_target_seq.infer_target_seq(variant_list), target_seq)
+
+    def test_incomplete_data(self):
+        target_seq = "CAATTTGGTTGGTCTGCTAATATGGAA"
+        variant_list = [Variant("n.1C>A"), Variant("n.2A>C"), Variant("n.3A>C"), Variant("n.4T>A"),
+                        Variant("n.5T>A"), Variant("n.6T>A"), Variant("n.7G>A"), Variant("n.8G>A"),
+                        Variant("n.9T>A"), Variant("n.10T>A"), Variant("n.11G>A"), Variant("n.12G>A"),
+                        Variant("n.13T>A"), Variant("n.14C>A"), Variant("n.15T>A"), Variant("n.16G>A"),
+                        Variant("n.17C>A"), Variant("n.18T>A"), Variant("n.19A>C"), Variant("n.20A>C"),
+                        Variant("n.21T>A"), Variant("n.22A>C"), Variant("n.23T>A"), Variant("n.24G>A"),
+                        Variant("n.25G>A"), Variant("n.26A>C")] #, Variant("n.27A>C")]
+
+        self.assertEqual(infer_target_seq.infer_target_seq(variant_list), target_seq)
+
+        target_seq = "CAATTTGGTTGGTCTGCTAATATGGAA"
+        variant_list = [Variant("n.1C>A"), Variant("n.2A>C"), Variant("n.3A>C"), Variant("n.4T>A"),
+                        #Variant("n.5T>A"), Variant("n.6T>A"), Variant("n.7G>A"), Variant("n.8G>A"),
                         Variant("n.9T>A"), Variant("n.10T>A"), Variant("n.11G>A"), Variant("n.12G>A"),
                         Variant("n.13T>A"), Variant("n.14C>A"), Variant("n.15T>A"), Variant("n.16G>A"),
                         Variant("n.17C>A"), Variant("n.18T>A"), Variant("n.19A>C"), Variant("n.20A>C"),
