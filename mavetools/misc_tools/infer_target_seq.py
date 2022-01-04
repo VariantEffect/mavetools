f397c314cccd7e0c59009643e93453cfae1f969f
harmatt <79935163+harmatt@users.noreply.github.com>
2022-01-04 15:16:36 -0800

    add amino acid dictionary

--- mavetools/misc_tools/infer_target_seq.py
+++ mavetools/misc_tools/infer_target_seq.py
@@ -24,6 +24,11 @@ def infer_target_seq(variant_list):
         there are variants for positions 1-10 and 12-15 but not 11). The function would also throw an error if
         there are conflicting target residues for the same position.
     """
+    aa_dict = {"A": "Ala", "C": "Cys", "D": "Asp", "E": "Glu", "F": "Phe",
+               "G": "Gly", "H": "His", "I": "Ile", "K": "Lys", "L": "Leu",
+               "M": "Met", "N": "Asn", "P": "Pro", "Q": "Gln", "R": "Arg",
+               "S": "Ser", "T": "Thr", "V": "Val", "W": "Trp", "Y": "Tyr",
+               "*": "Ter", "WTSYN": ""}
 
     # declare target_seq
     target_seq = ""
