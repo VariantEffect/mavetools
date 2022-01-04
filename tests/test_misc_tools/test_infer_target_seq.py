8b730318a55280fcbdf0c333eaf8929777a8471a
harmatt <79935163+harmatt@users.noreply.github.com>
2022-01-04 14:08:12 -0800

    edit hgvs format

--- tests/test_misc_tools/test_infer_target_seq.py
+++ tests/test_misc_tools/test_infer_target_seq.py
@@ -18,7 +18,7 @@ class Test(TestCase):
 
     def test_multi_var_single_sub(self):
         target_seq = "CAATTTGGTTGGTCTGCTAATATGGAA"
-        variant_list = [Variant("n.[1C>A,3A>C]"), Variant("n.2A>C"), Variant("n.4T>A"),
+        variant_list = [Variant("n.[1C>A;3A>C]"), Variant("n.2A>C"), Variant("n.4T>A"),
                         Variant("n.5T>A"), Variant("n.6T>A"), Variant("n.7G>A"), Variant("n.8G>A"),
                         Variant("n.9T>A"), Variant("n.10T>A"), Variant("n.11G>A"), Variant("n.12G>A"),
                         Variant("n.13T>A"), Variant("n.14C>A"), Variant("n.15T>A"), Variant("n.16G>A"),
