1d25b81e95c1c653365b755d7f491759468c307f
harmatt <79935163+harmatt@users.noreply.github.com>
2022-01-05 14:39:50 -0800

    update validate_dataset

--- mavetools/validators/validate.py
+++ mavetools/validators/validate.py
@@ -32,36 +32,37 @@ def validate_dataset(countfile=None, scorefile=None, scorejson=None):
 
     if scorefile is not None:
         # open scorefile
-        open(scorefile)
+        file_scorefile = open(scorefile, "r")
+
         # this one returns header
-        scoreheader = dataset_validators.read_header_from_io(file=scorefile)
+        #scoreheader = dataset_validators.read_header_from_io(file=scorefile)
 
         # if the header was returned, do these ones
-        dataset_validators.validate_has_hgvs_in_header(header=scoreheader)
-        dataset_validators.validate_at_least_one_additional_column(header=scoreheader)
-        dataset_validators.validate_header_contains_no_null_columns(header=scoreheader)
+        #dataset_validators.validate_has_hgvs_in_header(scorefile)
+        #dataset_validators.validate_at_least_one_additional_column(header=scoreheader)
+        #dataset_validators.validate_header_contains_no_null_columns(header=scoreheader)
 
-        dataset_validators.validate_scoreset_score_data_input(file=scorefile)
+        dataset_validators.validate_scoreset_score_data_input(file=file_scorefile)
 
         if scorejson is not None:
             # open scorejson
-            open(scorejson)
-            dataset_validators.validate_scoreset_json(dict_=scorejson)
+            file_scorejson = open(scorejson, "r")
+            dataset_validators.validate_scoreset_json(dict_=file_scorejson)
 
     if countfile is not None:
         # open countfile
-        open(countfile)
-        countheader = dataset_validators.read_header_from_io(file=countfile)
+        file_countfile = open(countfile, "r")
+        countheader = dataset_validators.read_header_from_io(file=file_countfile)
 
         # if the header was returned, do these ones
-        dataset_validators.validate_has_hgvs_in_header(header=countheader)
-        dataset_validators.validate_at_least_one_additional_column(header=countheader)
-        dataset_validators.validate_header_contains_no_null_columns(header=countheader)
+        #dataset_validators.validate_has_hgvs_in_header(header=countheader)
+        #dataset_validators.validate_at_least_one_additional_column(header=countheader)
+        #dataset_validators.validate_header_contains_no_null_columns(header=countheader)
 
-        dataset_validators.validate_scoreset_count_data_input(file=countfile)
+        dataset_validators.validate_scoreset_count_data_input(file=file_countfile)
 
-    if scorefile is not None and countfile is not None:
-        dataset_validators.validate_datasets_define_same_variants(scores=scorefile, counts=countfile)
+    #if scorefile is not None and countfile is not None:
+        #dataset_validators.validate_datasets_define_same_variants(scores=file_scorefile, counts=file_countfile)
 
 
 
