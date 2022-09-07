03ddbe384288fed184532f5b142e5a045fbe219a
harmatt <79935163+harmatt@users.noreply.github.com>
2022-09-07 10:12:39 -0700

    simple create dataset

---
+++ mavetools/easy_api/create_dataset.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from mavetools.client.client import Client
+
+
+def create_scoreset(new_scoreset, base_url, auth_token, scores_file_path, counts_file_path=None):
+    """
+
+    """
+    client = Client(base_url, auth_token=auth_token)
+    scores = pd.read_csv(scores_file_path)
+    if counts_file_path is not None:
+        counts = pd.read_csv(counts_file_path)
+    scoreset_urn = client.create_scoreset(new_scoreset, scores_df=scores, counts_df=counts)
+    return scoreset_urn
+
+
+def create_experiment(new_experiment, base_url, auth_token):
+    """
+
+    """
+    client = Client(base_url, auth_token=auth_token)
+    experiment_urn = client.create_experiment(new_experiment)
+    return experiment_urn
