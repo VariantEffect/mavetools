7f4c3543b3eefbbfdcf1e9c2144e18af67ad58fa
harmatt <79935163+harmatt@users.noreply.github.com>
2022-10-05 18:43:03 -0700

    dont require base url in easy api

--- mavetools/easy_api/get_dataset.py
+++ mavetools/easy_api/get_dataset.py
@@ -1,20 +1,20 @@
 from mavetools.client.client import Client
 
 
-def get_scoreset(scoreset_urn, base_url):
+def get_scoreset(scoreset_urn):
     """
 
     """
-    client = Client(base_url)
+    client = Client()
     scoreset = client.get_scoreset(scoreset_urn)
     return scoreset
 
 
-def get_experiment(experiment_urn, base_url):
+def get_experiment(experiment_urn):
     """
 
     """
-    client = Client(base_url)
+    client = Client()
     experiment = client.get_experiment(experiment_urn)
     return experiment
 
