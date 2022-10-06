a856f1052b6da39bd3fc131b8d04815f1471f809
harmatt <79935163+harmatt@users.noreply.github.com>
2022-10-05 18:34:08 -0700

    automatically set base url in easy api

--- mavetools/easy_api/create_dataset.py
+++ mavetools/easy_api/create_dataset.py
@@ -2,11 +2,11 @@ import pandas as pd
 from mavetools.client.client import Client
 
 
-def create_scoreset(new_scoreset, base_url, auth_token, scores_file_path, counts_file_path=None):
+def create_scoreset(new_scoreset, auth_token, scores_file_path, counts_file_path=None):
     """
 
     """
-    client = Client(base_url, auth_token=auth_token)
+    client = Client(auth_token=auth_token)
     scores = pd.read_csv(scores_file_path)
     if counts_file_path is not None:
         counts = pd.read_csv(counts_file_path)
@@ -14,10 +14,10 @@ def create_scoreset(new_scoreset, base_url, auth_token, scores_file_path, counts
     return scoreset_urn
 
 
-def create_experiment(new_experiment, base_url, auth_token):
+def create_experiment(new_experiment, auth_token):
     """
 
     """
-    client = Client(base_url, auth_token=auth_token)
+    client = Client(auth_token=auth_token)
     experiment_urn = client.create_experiment(new_experiment)
     return experiment_urn
