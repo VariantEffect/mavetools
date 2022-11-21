4e6f8fe79d6bc27ff16623c3d7586620c092ab64
harmatt <79935163+harmatt@users.noreply.github.com>
2022-11-21 12:57:00 -0800

    add async POST requests to easy api

--- mavetools/easy_api/create_dataset.py
+++ mavetools/easy_api/create_dataset.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import asyncio
+from aiohttp import ClientSession, TCPConnector
+
 from mavetools.client.client import Client
+from mavetools.client_async.client import Client as ClientAsync
 
 
 def create_scoreset(new_scoreset, auth_token, scores_file_path, counts_file_path=None):
@@ -14,6 +18,19 @@ def create_scoreset(new_scoreset, auth_token, scores_file_path, counts_file_path
     return scoreset_urn
 
 
+async def create_scoresets(scoreset_list, auth_token):
+    """
+
+    """
+    client = ClientAsync(auth_token=auth_token)
+    async with ClientSession(connector=TCPConnector(ssl=client.sslcontext)) as client.session:
+        r = await asyncio.gather(*[client.create_dataset(scoreset[0],
+                                                         "scoresets",
+                                                         scoreset[1],
+                                                         scoreset[2]) for scoreset in scoreset_list])
+    return r
+
+
 def create_experiment(new_experiment, auth_token):
     """
 
@@ -21,3 +38,13 @@ def create_experiment(new_experiment, auth_token):
     client = Client(auth_token=auth_token)
     experiment_urn = client.create_experiment(new_experiment)
     return experiment_urn
+
+
+async def create_experiments(experiment_list, auth_token):
+    """
+
+    """
+    client = ClientAsync(auth_token=auth_token)
+    async with ClientSession(connector=TCPConnector(ssl=client.sslcontext)) as client.session:
+        r = await asyncio.gather(*[client.create_dataset(experiment, "experiments") for experiment in experiment_list])
+    return r
